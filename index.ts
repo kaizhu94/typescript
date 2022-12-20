bf30f0e119a57d6942cb806d93580be7b9763115
kaizhu94 <zhukai0803@gmail.com>
2022-12-20 13:17:57 -0500

    Types

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
 console.log('hello world');
+
+const isOpen: boolean = false;
+const myName: string = "Kai";
+const myAge: number = 29;
+
+const list: number[] = [0, 1, 2];
+
+const me: [string, number, boolean]= ["Kai", 29, false];
+
+enum Job { 
+    WebDev, 
+    WebDesigner, 
+    PM }
+const job:Job = Job.WebDev;
+
+const phone: any = "Pixel";
+const tablet: never = 3;
